87efff7f968b900f22ab021c43456ef483f3f795
felipemuner <felipe.muner@gmail.com>
2021-06-18 12:25:16 +0700

    installing graphql to fix bug module not found

---
+++ pages/api/graphql.js
@@ -0,0 +1,15 @@
+import { ApolloServer } from "apollo-server-micro";
+import { typeDefs } from "./schemas";
+import { resolvers } from "./resolvers";
+
+console.log('felipeee',typeDefs, resolvers)
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default apolloServer.createHandler({ path: "/api/graphql" });
